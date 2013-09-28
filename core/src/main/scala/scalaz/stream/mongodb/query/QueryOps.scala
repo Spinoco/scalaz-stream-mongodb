852b877a38f5f0c3f96ca333c1b5cc53535b68ee
Pavel Chlupacek <pavel.chlupacek@spinoco.com>
2013-09-28 18:56:57 +0200

    Basic aggregation commands added + tested

--- core/src/main/scala/scalaz/stream/mongodb/query/QueryOps.scala
+++ core/src/main/scala/scalaz/stream/mongodb/query/QueryOps.scala
@@ -6,6 +6,12 @@ import scalaz.stream.mongodb.channel.ChannelResult
 import scalaz.stream.mongodb.collectionSyntax._
 import com.mongodb.{DBCollection, DBObject}
 import scalaz.stream.mongodb.aggregate.{MapReduceDefinition, PipelineOperator}
+import scalaz.concurrent.Task
+import scalaz.stream.Process._
+import scalaz.stream.Process
+import scalaz.stream.mongodb.bson.BSONSerializable
+import scala.util.{Failure, Success}
+import collection.JavaConverters._
 
 
 trait QueryOps extends Ops[Query] {
@@ -49,10 +55,38 @@ trait QueryOps extends Ops[Query] {
   def mapReduce(mapReduce: MapReduceDefinition): ChannelResult[DBCollection, DBObject] = ???
 
   /** Counts the documents that matched the query **/
-  def count: ChannelResult[DBCollection, Long] = ???
+  def count: ChannelResult[DBCollection, Long] = ChannelResult {
+    c => Task.now {
+      self.readPreference match {
+        case Some(rp) => c.count(self.bq.o, rp.asMongoDbReadPreference)
+        case None => c.count(self.bq.o)
+      }
+    }
+  }
 
   /** Returns all distinct values of key in query **/
-  def distinct[A](key: String): ChannelResult[DBCollection, A] = ???
+  def distinct[A: BSONSerializable](key: String): ChannelResult[DBCollection, A] = ChannelResult {
+    import Task._
+    val serializable = implicitly[BSONSerializable[A]]
+    //todo: current java driver is non-strict on distinct operation, we have to reconsider if it can be implemented lazily 
+    Process.wrap {
+      now {
+        c: DBCollection => now {
+          emitAll(
+            c.distinct(key
+              , self.bq.o
+              , self.readPreference.map(_.asMongoDbReadPreference).getOrElse(c.getReadPreference)).asScala)
+          .evalMap {
+            raw => serializable.tryGet(raw) match {
+              case Success(a) => Task.now(a)
+              case Failure(e) => Task.fail(e)
+            }
+
+          }
+        }
+      }
+    }: Channel[Task, DBCollection, Process[Task, A]]
+  }
 
 
   /** Appends results of two queries together **/
--- core/src/test/scala/scalaz/stream/mongodb/aggregate/BasicAggregationSpec.scala
+++ core/src/test/scala/scalaz/stream/mongodb/aggregate/BasicAggregationSpec.scala
@@ -4,16 +4,16 @@ import org.specs2.Specification
 import org.specs2.specification.Snippets
 
 import scalaz.stream.mongodb.collectionSyntax._
+import scalaz.stream.mongodb.channel.ChannelResult
+import com.mongodb.DBCollection
+import org.specs2.matcher.MatchResult
+import scalaz.stream.mongodb.MongoRuntimeSpecification
+import scalaz.stream.mongodb.query.ReadPreference
+import scalaz.stream.mongodb.bson.BSONSerializable
 
-/**
- *
- * User: pach
- * Date: 9/28/13
- * Time: 5:19 AM
- * (c) 2011-2013 Spinoco Czech Republic, a.s.
- */
-class BasicAggregationSpec extends Specification with Snippets{
-  
+
+class BasicAggregationSpec extends Specification with Snippets with MongoRuntimeSpecification {
+
   def is =
     s2"""
     
@@ -23,10 +23,47 @@ ${"Generic aggregation functions"}
 Apart from map-reduce and aggregation pipeline Mongo Streams supports basic mongodb aggregation functions. Supported
 aggregation functions are: 
 
-* count of document matching supplied query ${ snippet { query() count}} 
-* getting distinct values for supplied key ${ snippet { query() distinct("key")}}       
+* count of document matching supplied query  ${ snippet { query("key2" >= 3) count }}                $count
+* count of document with supplied query and read preference:  ${ snippet { query("key2" >= 3) from ReadPreference.Nearest count }}    
+* count of all documents     ${ snippet { query() count }}                $countAll
+* getting distinct values for supplied key ${ snippet { (query() distinct ("key")): ChannelResult[DBCollection, String] }}      $distinct 
         
       
     """
+
+
+  def count = AggregationResult(query("key2" >= 3) count).verify {
+    case a :: Nil => a must_== 2
+    case other => ko("unexpected " + other)
+  }
+
+  def countAll = AggregationResult(query() count).verify {
+    case a :: Nil => a must_== 4
+    case other => ko("unexpected " + other)
+  }
+
+  def distinct = AggregationResult[String](query("key2" >= 2).distinct("key")).verify {
+    result => result.toSet must haveTheSameElementsAs(Set("b", "c"))
+  }
+
+
+  case class AggregationResult[A: BSONSerializable](ch: ChannelResult[DBCollection, A]) {
+
+    val mongo = new WithMongoCollection {}
+
+
+    def verify(f: Seq[A] => MatchResult[Any]): MatchResult[Any] = {
+
+      mongo.collection.insert(BSONObject("key" -> "a", "key2" -> 1))
+      mongo.collection.insert(BSONObject("key" -> "b", "key2" -> 2))
+      mongo.collection.insert(BSONObject("key" -> "b", "key2" -> 3))
+      mongo.collection.insert(BSONObject("key" -> "c", "key2" -> 4))
+
+
+      f((mongo.collection through ch).collect.run.toList)
+    }
+
+  }
+
 
 }
