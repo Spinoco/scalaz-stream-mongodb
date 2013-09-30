a806cfd118ed23352169e3dfebfa818ea2cc2cd1
Pavel Chlupacek <pavel.chlupacek@spinoco.com>
2013-09-30 08:07:46 +0200

    LimitPipeLine added to git

--- core/src/main/scala/scalaz/stream/mongodb/aggregate/LimitPipeLine.scala
+++ core/src/main/scala/scalaz/stream/mongodb/aggregate/LimitPipeLine.scala
@@ -1,14 +1,9 @@
 package scalaz.stream.mongodb.aggregate
 
-/**
- *
- * User: pach
- * Date: 9/26/13
- * Time: 7:30 AM
- * (c) 2011-2013 Spinoco Czech Republic, a.s.
- */
+import com.mongodb.{BasicDBObject, DBObject}
 
 /** limits returned results from pipeline **/
 case class LimitPipeline (count:Int) extends PipelineOperator    {
-  lazy val asDBObject = ???
+  lazy val asDBObject : DBObject =
+    new BasicDBObject().append("$limit", count)
 }
